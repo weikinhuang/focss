bb2276c2c25cb70c257b6639b2a9e39faceb6fc7
Alex Lee <aintaer@gmail.com>
2015-01-21 15:05:31 -0500

    Selector strategy 0: mark and sweep
    This is the basic strategy for applying styles to elements.
    Should allow for elements to change properties and unapply styles.

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-define(['./lib/Rules'], function(Rules) {
+define(['nbd/Class', './lib/Rules'], function(Class, Rules) {
   'use strict';
 
-  Rules.attach();
-  return Rules;
+  var Focss = Class.extend({
+    init: function() {
+      this.rules = new Rules();
+      this.rules.bind();
+    },
+
+    /**
+     * Insert a focss rule
+     * @param selector {String} CSS selector/dynamic selector
+     * @param spec {Object} key/value map of CSS property to expression
+     */
+    insert: function(selector, spec) {
+      if (typeof selector === "object") {
+        for (var s in selector) {
+          if (selector.hasOwnProperty(s)) {
+            this.insert(s, selector[s]);
+          }
+        }
+        return;
+      }
+
+      this.rules.insert(selector, spec);
+    },
+
+    /**
+     * Run the current set of rules against state data
+     * Previous state data is overridden.
+     * @param data {Object} state data
+     */
+    process: function(data) {
+      this.rules.process(data);
+    },
+
+    destroy: function() {
+      this.rules.destroy();
+      this.rules = null;
+    }
+  }, {
+    displayName: 'Focss'
+  });
+
+  return Focss;
 });
