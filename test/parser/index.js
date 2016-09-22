b5134dab1c06f9f428ccf4c35418edf1a619897b
Kai Cataldo <kaicataldo@gmail.com>
2016-09-22 17:44:05 -0400

    Add support for parsing variables

--- test/parser/index.js
+++ test/parser/index.js
@@ -6,14 +6,18 @@ import { parse, parseSync } from '../../src/parser';
 import Focss from '../../src';
 
 describe('parser', function() {
-  describe('should return an array of parsed Focss descriptors', function() {
-    function checkDescriptors(parsedDescriptors) {
-      expect(Array.isArray(parsedDescriptors)).toBeTruthy();
-      expect(parsedDescriptors.length).toEqual(2);
+  describe('should return an array of parsed Focss descriptors and object of variables', function() {
+    function checkDescriptors({ descriptors, variables }) {
+      const varsIsObject = variables !== null && typeof variables === 'object' && !Array.isArray(variables);
+      expect(varsIsObject).toBeTruthy();
+      expect(Object.keys(variables).length).toEqual(1);
+      expect(Array.isArray(descriptors)).toBeTruthy();
+      expect(descriptors.length).toEqual(2);
     }
 
-    const styles = `.foo {
-                      max-width: bar;
+    const styles = `$foo: 100;
+                    .bar {
+                      max-width: $foo;
                     }
                     .baz {
                       width: qux;
@@ -21,15 +25,15 @@ describe('parser', function() {
 
     it('when parsed with parse', function(done) {
       parse(styles)
-      .then(({ focssDescriptors }) => {
-        checkDescriptors(focssDescriptors);
+      .then(({ focss }) => {
+        checkDescriptors(focss);
       })
       .then(done, done.fail);
     });
 
     it('when parsed with parseSync', function() {
-      const focssDescriptors = parseSync(styles);
-      checkDescriptors(focssDescriptors);
+      const parsedDescriptors = parseSync(styles);
+      checkDescriptors(parsedDescriptors);
     });
   });
 
@@ -73,15 +77,15 @@ describe('parser', function() {
 
     it('when parsed with parse', function(done) {
       parse(styles)
-      .then(({ focssDescriptors }) => {
-        expect(focssDescriptors).toEqual(result);
+      .then(({ focss: { descriptors } }) => {
+        expect(descriptors).toEqual(result);
       })
       .then(done, done.fail);
     });
 
     it('when parsed with parseSync', function() {
-      const focssDescriptors = parseSync(styles);
-      expect(focssDescriptors).toEqual(result);
+      const { descriptors } = parseSync(styles);
+      expect(descriptors).toEqual(result);
     });
   });
 
@@ -125,15 +129,15 @@ describe('parser', function() {
 
     it('when parsed with parse', function(done) {
       parse(styles)
-      .then(({ focssDescriptors }) => {
-        expect(focssDescriptors).toEqual(result);
+      .then(({ focss: { descriptors } }) => {
+        expect(descriptors).toEqual(result);
       })
       .then(done, done.fail);
     });
 
     it('when parsed with parseSync', function() {
-      const focssDescriptors = parseSync(styles);
-      expect(focssDescriptors).toEqual(result);
+      const { descriptors } = parseSync(styles);
+      expect(descriptors).toEqual(result);
     });
   });
 
@@ -153,15 +157,15 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
@@ -187,15 +191,15 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
@@ -214,15 +218,15 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
@@ -241,15 +245,15 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
@@ -268,15 +272,15 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
@@ -295,15 +299,15 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
@@ -322,20 +326,144 @@ describe('parser', function() {
 
       it('when parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          expect(focssDescriptors).toEqual(result);
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
         })
         .then(done, done.fail);
       });
 
       it('when parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        expect(focssDescriptors).toEqual(result);
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
       });
     });
 
-    describe('calling toString() should return processed styles', function() {
-      function processAndValidate(focssDescriptors, result) {
+    describe('Focss variables', function() {
+      const styles = `$foo: 600;
+                      $bar: '#123ABC';
+
+                      .test-static {
+                        prop: ($foo * 2) + 'px';
+                      }
+                      .test-method {
+                        prop: darken($bar, 16);
+                      }
+                      .test-jquery {
+                        prop: $.extend(true, {}, some.data);
+                      }
+                      .test-regex {
+                        prop: /^test$/.test(some.other.data) ? foo : bar;
+                      }`;
+      const result = [
+        {
+          selector: '.test-static',
+          rules: {
+            prop: '(600 * 2) + \'px\''
+          }
+        },
+        {
+          selector: '.test-method',
+          rules: {
+            prop: 'darken(\'#123ABC\', 16)'
+          }
+        },
+        {
+          selector: '.test-jquery',
+          rules: {
+            prop: '$.extend(true, {}, some.data)'
+          }
+        },
+        {
+          selector: '.test-regex',
+          rules: {
+            prop: '/^test$/.test(some.other.data) ? foo : bar'
+          }
+        }
+      ];
+
+      it('when parsed with parse', function(done) {
+        parse(styles)
+        .then(({ focss: { descriptors } }) => {
+          expect(descriptors).toEqual(result);
+        })
+        .then(done, done.fail);
+      });
+
+      it('when parsed with parseSync', function() {
+        const { descriptors } = parseSync(styles);
+        expect(descriptors).toEqual(result);
+      });
+
+      describe('should throw an error when a variable is not defined before use', function() {
+        const undefinedVarStyles = `.missing-var {
+                                     prop: $not-defined;
+                                   }`;
+        const errMsg = 'Variable $not-defined is not defined. (2:38)';
+
+        it('when parsed with parse', function(done) {
+          parse(undefinedVarStyles)
+          .catch(({ message }) => {
+            expect(message).toEqual(errMsg);
+          })
+          .then(done, done.fail);
+        });
+
+        it('when parsed with parseSync', function() {
+          expect(() => {
+            parseSync(undefinedVarStyles);
+          }).toThrowError(errMsg);
+        });
+      });
+    });
+  });
+
+  describe('calling toString() should return processed styles', function() {
+    describe('when global variables are provided', function() {
+      function processAndValidate({ descriptors }, result) {
+        const fox = new Focss();
+        const data = {
+          className: 'foo',
+          width1: 200,
+          width2: 600,
+          num1: 3,
+          num2: 5,
+          opacity: .6,
+          foo: [
+            { id: 1, num1: 1, num2: 4, num3: 2, float: 'left' },
+            { id: 2, num1: 2, num2: 3, num3: 3, float: 'right' },
+            { id: 3, num1: 3, num2: 6, num3: 4, float: 'none' },
+            { id: 4, num1: 4, num2: 2, num3: 1, float: 'left' }
+          ],
+          baz: [
+            { id: 1, width: 400 },
+            { id: 2, width: 600 }
+          ]
+        };
+
+        fox.insert(descriptors);
+        expect(fox.toString(data)).toEqual(result);
+      }
+
+      const variables = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'variables.focss'), { encoding: 'utf8' });
+      const styles = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'stylesWithGlobalVars.focss'), { encoding: 'utf8' });
+      const result = '.foo{width:300px;}.foo class-name:hover{max-width:800%;}nav .foo a.active, nav .foo a.__fake{margin-left:200px;}.foo 3{opacity:0;}.class1[data-id="3"]{float:none;margin-top:18px;width:18px;}.class1[data-id="4"]{float:left;margin-top:8px;width:8px;}.class2[data-id="1"]{max-width:1800px;}.class2[data-id="2"]{max-width:1800px;}@media screen and (min-width: 800px) and (max-width: 300px){.foo{width:300px;}.foo class-name:hover{max-width:800%;}nav .foo a.active, nav .foo a.__fake{margin-left:200px;}.foo 3{opacity:0;}.class1[data-id="3"]{float:none;margin-top:18px;width:18px;}.class1[data-id="4"]{float:left;margin-top:8px;width:8px;}.class2[data-id="1"]{max-width:1800px;}.class2[data-id="2"]{max-width:1800px;}}';
+
+      it('when inserted descriptors are parsed with parse', function(done) {
+        parse(variables)
+        .then(({ focss: { variables: parsedVars } }) => parse(styles, parsedVars))
+        .then(({ focss: parsedDescriptors }) => processAndValidate(parsedDescriptors, result))
+        .then(done, done.fail);
+      });
+
+      it('when inserted descriptors are parsed with parseSync', function() {
+        const { variables: parsedVars } = parseSync(variables);
+        const parsedDescriptors = parseSync(styles, parsedVars);
+        processAndValidate(parsedDescriptors, result);
+      });
+    });
+
+    describe('when no global variables are provided', function() {
+      function processAndValidate({ descriptors }, result) {
         const fox = new Focss();
         const data = {
           width1: 200,
@@ -355,24 +483,22 @@ describe('parser', function() {
           ]
         };
 
-        fox.insert(focssDescriptors);
+        fox.insert(descriptors);
         expect(fox.toString(data)).toEqual(result);
       }
 
       const styles = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'styles.focss'), { encoding: 'utf8' });
-      const result = '.foo{width:200px;}.foo:hover{max-width:800%;}nav .foo a.active, nav .foo a.__fake{margin-left:200px;}.foo 3{opacity:1;}.class1[data-id="3"]{float:none;margin-top:18px;width:18px;}.class1[data-id="4"]{float:left;margin-top:8px;width:8px;}.class2[data-id="1"]{max-width:400px;}.class2[data-id="2"]{max-width:600px;}@media screen and (max-width: 300px){.foo{width:200px;}.foo:hover{max-width:800%;}nav .foo a.active, nav .foo a.__fake{margin-left:200px;}.foo 3{opacity:1;}.class1[data-id="3"]{float:none;margin-top:18px;width:18px;}.class1[data-id="4"]{float:left;margin-top:8px;width:8px;}.class2[data-id="1"]{max-width:400px;}.class2[data-id="2"]{max-width:600px;}}';
+      const result = '.foo{width:300px;}.class-name:hover{max-width:800%;}nav .foo a.active, nav .foo a.__fake{margin-left:200px;}.foo 3{opacity:1;}.class1[data-id="3"]{float:none;margin-top:18px;width:18px;}.class1[data-id="4"]{float:left;margin-top:8px;width:8px;}.class2[data-id="1"]{max-width:400px;}.class2[data-id="2"]{max-width:600px;}@media screen and (min-width: 800px) and (max-width: 300px){.foo{width:300px;}.class-name:hover{max-width:800%;}nav .foo a.active, nav .foo a.__fake{margin-left:200px;}.foo 3{opacity:1;}.class1[data-id="3"]{float:none;margin-top:18px;width:18px;}.class1[data-id="4"]{float:left;margin-top:8px;width:8px;}.class2[data-id="1"]{max-width:400px;}.class2[data-id="2"]{max-width:600px;}}';
 
       it('when inserted descriptors are parsed with parse', function(done) {
         parse(styles)
-        .then(({ focssDescriptors }) => {
-          processAndValidate(focssDescriptors, result);
-        })
+        .then(({ focss: parsedDescriptors }) => processAndValidate(parsedDescriptors, result))
         .then(done, done.fail);
       });
 
       it('when inserted descriptors are parsed with parseSync', function() {
-        const focssDescriptors = parseSync(styles);
-        processAndValidate(focssDescriptors, result);
+        const parsedDescriptors = parseSync(styles);
+        processAndValidate(parsedDescriptors, result);
       });
     });
   });
